58ee1a18147f7f38d7f229cda34000a8eaf65229
2Archy34 <132705676+2Archy34@users.noreply.github.com>
2023-05-06 16:37:59 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch response from server
 
-  const response = await fetch('https://gpt4-backend-ahsp.onrender.com:3000', {
+  const response = await fetch('https://gpt4-backend-ahsp.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
